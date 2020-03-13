ab049b1081914ed7e57ec7c5c19776503ecd02a9
안유찬 노트북 <anyc123@hanmail.net>
2020-03-14 04:12:29 +0900

    Add Chat_UI
    .

--- Assets/TCP_Master/TCP_Master.cs
+++ Assets/TCP_Master/TCP_Master.cs
@@ -7,41 +7,26 @@ using YC;
 
 public class TCP_Master : MonoBehaviour
 {
-    public string input_chat;
+    public static TCP_Master Inst;
 
-    private void Start()
+    YC_TCP_Master tcp;
+
+    private void Awake()
     {
+        Inst = this;
+
         ioev.Map<test_t>(0);
 
-        ioev.Signal((test_t t) =>
-        {
-            Debug.Log(t.chat_data);
-        });
+        tcp = new YC_TCP_Master("127.0.0.1", 51234);
+    }
 
-        YC_TCP_Master tcp = new YC_TCP_Master("127.0.0.1", 51234);
+    private void Update()
+    {
+        tcp.run();
+    }
 
-        Thread t1 = new Thread(new ThreadStart(() => { while (true) tcp.run(); }));
-        t1.Start();
-
-        while (true)
-        {
-            var input = input_chat;
-
-            test_t t = new test_t();
-            var b = input.ToCharArray();
-
-            if (b.Length >= 100)
-            {
-                Debug.Log("100자 이상의 문자는 전송할 수 없습니다.");
-                continue;
-            }
-            Array.Copy(b, 0, t.chat_data, 0, b.Length);
-
-            for (int i = 0; i < 1000; i++)
-            {
-                Thread.Sleep(10);
-                tcp.send(t);
-            }
-        }
+    public void Send(IPacket_t p)
+    {
+        tcp.send(p);
     }
 }
---
+++ Assets/UI/Chat/YC_Chat.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using YC;
+
+public class YC_Chat : MonoBehaviour
+{
+    [SerializeField] InputField input_filed;
+    [SerializeField] Text text_area;
+    [SerializeField] string buf = "";
+    void Start()
+    {
+        ioev.Signal((test_t t) =>
+        {
+            buf += "\n" + new string((from c in t.chat_data
+                                      where c != '\0'
+                                      select c).ToArray());
+            text_area.text = $"{buf}";
+        });
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            var input = input_filed.text;
+            if (input == "") return;
+            
+            input_filed.text = "";
+            test_t t = new test_t();
+            var b = input.ToCharArray();
+
+            if (b.Length >= 100)
+            {
+                Debug.Log("100자 이상의 문자는 전송할 수 없습니다.");
+                return;
+            }
+            Array.Copy(b, 0, t.chat_data, 0, b.Length);
+
+            TCP_Master.Inst.Send(t);
+        }
+    }
+}
